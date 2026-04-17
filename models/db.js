20b7b87cc8804ae2e0420c931e06a17ea49c6446
Caroline Tenorio <carololiveiratenorio21@gmail.com>
2026-04-17 20:38:44 +0100

    modificado

--- models/db.js
+++ models/db.js
@@ -1,11 +1,5 @@
 const mysql = require("mysql2/promise");
 
-const db = mysql.createPool({
-  uri: process.env.MYSQL_PUBLIC_URL,
-  charset: "utf8mb4",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const db = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 
 module.exports = db;
